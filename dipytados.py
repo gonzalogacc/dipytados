d6f176c8ed1b4977a68b9ff4aca8a994e4aefae4
Gonzalo Garcia Accinelli <gonzalogacc@gmail.com>
2013-05-01 05:03:39 -0300

    cambios dipytados.py

--- dipytados.py
+++ dipytados.py
@@ -10,13 +10,21 @@ import re
 
 
 class sesion():
-	'''
+	"""
 	Clase que contiene una sesion de la camara de diputados
-	'''
+	"""
 	def __init__(self, periodo, reunion, sesion, tipo_sesion, fecha):
-		'''
-		Variables propias de la sesion
-		'''
+		"""
+		Almacena las variables propias de la sesion
+		
+		Argumentos
+		-------------------------
+		periodo: periodo que corresponde a la sesion
+		reunion: numero de reunion
+		sesion: sesion de la reunion y el periodo 
+		tipo_sesion: si es ordinaria o extraordinaria
+		fecha: fecha en formato dd/mm/aaaa
+		"""
 		self.periodo = periodo
 		self.reunion = reunion
 		self.sesion = sesion
@@ -25,17 +33,17 @@ class sesion():
 		
 		self.html_version_taquigrafica = None
 		self.dialogo = None
+		self.intervenciones = {}
 		
 	def getVersionTaquigrafica(self, file = None):
-		'''
-		(int, int) -> str
-		periodo, reunion -> texto de la version taquigrafica
-		
-		Devuelve el texto de la version taquigrafica dado un numero de 
-		periodo y reunion
+		"""
+		Devuelve el texto de la version taquigrafica dado un numero de periodo y reunion
 		Construye una url para consulta de la pagina de diputados y la parsea
 		
-		'''
+		Arguemntos
+		-------------------------
+		file: si hay una ruta a un archivo intenta parsear la version taquigrafica desde ahi sino la saca de internet
+		"""
 		
 		if file != None:
 			self.version_taquigrafica = open('/home/gonza/Escritorio/dipytados/sourcesesion_definitiva.html', 'r').read()
@@ -52,12 +60,11 @@ class sesion():
 			self.html_version_taquigrafica = version_taquigrafica
 	
 	def getDialogo(self):
-		'''
-		(srt parsed_page) -> str cadena de texto con toda la conversacion de la sesion
-		
+		"""		
 		Toma el source html de la pagina y usa beautiful soup para sacar las etiquetas span
 		luego concatena el texto y lo devuelve
-		'''
+		
+		"""
 		##print self.html_version_taquigrafica
 		bsp = BeautifulSoup(self.html_version_taquigrafica)
 		
@@ -70,13 +77,11 @@ class sesion():
 		self.dialogo = texto
 
 	def getDiputados(self):
-		'''
-		(str) -> list
-		
-		Toma una conversacion del senado y extrae las menciones a los diputados que participan hablando en la sesion.
+		"""
+		Toma la conversacion del senado y extrae las menciones a los diputados que participan hablando en la sesion.
 		Reconoce los diputados como el texto qu esta entre un 'sr.' o 'sra.' y un '.-'
 		Devuelve una lista de tuplas con los nombres, el inicio de la posicion dle nombre y el final
-		'''
+		"""
 		
 		prog = re.compile('(sr.|sra.).*,*(\.-)', re.IGNORECASE)
 		result = prog.finditer(self.dialogo)
@@ -87,23 +92,41 @@ class sesion():
 		
 		return lista_diputados
 	
+	def getIntervencionesDiputados (self):
+		""" Devuelve cada una de las intervenciones por separado dado un texto de la sesion """
+		prog_indices = re.compile('(sr.|sra.).*', re.IGNORECASE)
+		prog_nombre = re.compile('(sr.|sra.).*,*(\.-)', re.IGNORECASE)
+
+		result = prog_indices.finditer(self.dialogo)
+		
+		indices = []		
+		for i in result:
+			indices.append(i.span()[0])
+		
+		for indice in range(len(indices)-1):
+			
+			inicio, final = prog_nombre.match(self.dialogo[indices[indice]:indices[indice+1]]).span()
+			
+			discurso = self.dialogo[indices[indice]:indices[indice+1]]
+			nombre = discurso[inicio:final]
+			self.intervenciones[nombre] = append(discurso)
 		
 	def getActaVotacion(self):
-		'''
+		"""
 		Toma la url de un acta de votacion y guarda el archivo en un 
 		directorio temporal para el posterior procesamineto
-		'''
+		"""
 		
 
 	def getVotacionesSesion(self, anio, reunion):
-		'''
+		"""
 		(int, int) -> lista_votaciones
 		periodo, reunion -> lista_votaciones
 		
 		Devuelve una lista de objetos votaciones dados un periodo y una reunion
 		Lista las url de las votaciones de la pagina que coinciden con el periodo y reunion dadas
 		
-		'''
+		"""
 		
 		url = 'http://www.diputados.gov.ar/secadmin/ds_electronicos/periodo/'+str(anio)+'/index.html'
 		response = urllib2.urlopen(url)
@@ -120,39 +143,23 @@ class sesion():
 			if candidato[-6:-4] == reunion:
 				links_votaciones.append(candidato)
 		
+		return links_votaciones
 		
 	
 	def getVotacion(self, url):
-		'''
+		"""
 		(reunion, periodo, expediente) -> objeto votacion
 		
 		Devuelve un objeto que contiene una matriz de diputado, partido 
 		al que pertenece y votacion que realizo y tambien una identificacion
 		en referencia a lo que se voto
 		
-		'''
-
-class votacion(sesion):
-	'''
-	Objeto que contiene la informacion de una votacion expandir explicacion
-	'''
-	def __init__(self):
-		self.expediente = None
-		self.matriz_votacion = None
-	
-	def getArchivoVotacion():
-		'''
-		obtiene el archivo del resultado de la votacion y lo guarda en el disco?
-		'''
-	def construirMatriz_votacion():
-		'''
-		Devuelve una matriz de votacion dado un archivo de resultado de votacion
-		'''
+		"""
 	
 class diputado(sesion):
-	'''
+	"""
 	Objeto que contiene la informacion de un diputado en una sesion dada
-	'''
+	"""
 	def __init__(self):
 		self.nombre = None
 		self.intervenciones = None
@@ -160,36 +167,53 @@ class diputado(sesion):
 		self.presente = None
 		
 	def getIntervenciones():
-		'''
-		
+		"""
 		Devuelve una cadena de texto que es todo lo que dijo el diputado
 		concatenado dado el nombre del diputado
-		'''
+		"""
+
+class votacion(sesion):
+	"""
+	Objeto que contiene la informacion de una votacion expandir explicacion
+	"""
+	def __init__(self):
+		self.expediente = None
+		self.matriz_votacion = None
+	
+	def getArchivoVotacion():
+		"""
+		obtiene el archivo del resultado de la votacion y lo guarda en el disco?
+		"""
+	def construirMatriz_votacion():
+		"""
+		Devuelve una matriz de votacion dado un archivo de resultado de votacion
+		"""
 
 ##class listaVotaciones():
-	##'''
+	##"""
 	##Coleccion de elementos votacion
-	##'''	
+	##"""	
 	##def __init__():
 		##self.
 			##
 ##class listaDiputados():
-	##'''
+	##"""
 	##Coleccion de elementos diputados
-	##'''
+	##"""
 	##
 	##def __init__():
 		##self.
 		##
 
 
-ss =  sesion(129, 10, 7, 'especial', '30/11/2011')
-ss.getVersionTaquigrafica()
+if __name__ == '__main__':
+	ss =  sesion(129, 10, 7, 'especial', '30/11/2011')
+	ss.getVersionTaquigrafica()
 
-ss.getDialogo()
-##print ss.dialogo
-
-for i in ss.getDiputados():
-	print i
+	ss.getDialogo()
+	ss.getIntervencionesDiputados()
 	
-
+	for i in ss.intervenciones.keys():
+		print ss.intervenciones[i]
+		raw_input()
+	
